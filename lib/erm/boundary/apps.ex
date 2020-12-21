329f87b484b7673ce631cb82fe5b1158fdd69112
Gabriel Bellido Perez <gabrielete@gmail.com>
2020-12-21 21:32:56 +0100

    Add remove configuration of locally in code

--- lib/erm/boundary/apps.ex
+++ lib/erm/boundary/apps.ex
@@ -2,47 +2,6 @@ defmodule Erm.Boundary.Apps do
   alias Erm.Core.Action
   alias Erm.Core.Application
 
-  def registered_actions do
-    %{
-      "Locally" => [
-        Action.new(:add_store, :internal, Erm.Core.Actions.Locally.AddStore),
-        Action.new(:update_store, :internal, Erm.Core.Actions.Locally.UpdateStore),
-        Action.new(
-          :add_product_to_category,
-          :internal,
-          Erm.Core.Actions.Locally.AddProductToCategory
-        ),
-        Action.new(:add_product, :internal, Erm.Core.Actions.Locally.AddProduct),
-        Action.new(:update_product, :internal, Erm.Core.Actions.Locally.UpdateProduct),
-        Action.new(:add_stock, :internal, Erm.Core.Actions.Locally.AddStock),
-        Action.new(
-          :add_product_category,
-          :internal,
-          Erm.Core.Actions.Locally.AddProductCategory
-        ),
-        Action.new(
-          :update_product_category,
-          :internal,
-          Erm.Core.Actions.Locally.UpdateProductCategory
-        ),
-        Action.new(
-          :remove_product_category,
-          :internal,
-          Erm.Core.Actions.Locally.RemoveProductCategory
-        ),
-        Action.new(:remove_product, :internal, Erm.Core.Actions.Locally.RemoveProduct),
-        Action.new(:remove_store, :internal, Erm.Core.Actions.Locally.RemoveStore),
-        Action.new(
-          :remove_product_from_category,
-          :internal,
-          Erm.Core.Actions.Locally.RemoveProductFromCategory
-        ),
-        Action.new(:remove_stock, :internal, Erm.Core.Actions.Locally.RemoveStock),
-        Action.new(:update_stock, :internal, Erm.Core.Actions.Locally.UpdateStock)
-      ]
-    }
-  end
-
   def get_application(app_name) do
     case :ets.lookup(:erm_apps, app_name) do
       [] ->
@@ -102,10 +61,6 @@ defmodule Erm.Boundary.Apps do
       {:ok, content} -> content
       {:error, _} -> "{}"
     end
-  end
-
-  def get_actions(app_name) do
-    Map.get(registered_actions(), app_name, [])
   end
 
   def get_persistence(_app_name) do
