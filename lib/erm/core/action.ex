ba56c3c65f8eadd85f81a41ec2d02fbc4d2a0636
Gabriel Bellido Perez <gabrielete@gmail.com>
2020-12-21 22:01:48 +0100

    Add use input on id search on maps or raw values

--- lib/erm/core/action.ex
+++ lib/erm/core/action.ex
@@ -53,14 +53,14 @@ defmodule Erm.Core.Action do
   def run_action(
         %__MODULE__{
           type: :instructions,
-          input: _input,
+          input: input,
           instructions: instructions,
           output: output
         },
         application,
         params
       ) do
-    run_steps(application.name, params, instructions)
+    run_steps(application.name, input, params, instructions)
     |> return_output(output)
   end
 
@@ -75,17 +75,17 @@ defmodule Erm.Core.Action do
 
   defp return_output(error, _output), do: error
 
-  defp run_steps(app_name, params, steps) do
+  defp run_steps(app_name, input, params, steps) do
     steps
     |> Enum.reduce(Ecto.Multi.new(), fn step, multi ->
       Ecto.Multi.run(multi, step["name"], fn repo, bagage ->
         case step["instruction"] do
           "add_entity" -> add_entity(app_name, step, params, bagage, repo)
-          "update_entity" -> update_entity(app_name, step, params, bagage, repo)
-          "add_relation" -> add_relation(app_name, step, params, bagage, repo)
-          "update_relation" -> update_relation(app_name, step, params, bagage, repo)
-          "delete_relation" -> delete_relation(app_name, step, params, bagage, repo)
-          "delete_entity" -> delete_entity(app_name, step, params, bagage, repo)
+          "update_entity" -> update_entity(app_name, step, input, params, bagage, repo)
+          "add_relation" -> add_relation(app_name, step, input, params, bagage, repo)
+          "update_relation" -> update_relation(app_name, step, input, params, bagage, repo)
+          "delete_relation" -> delete_relation(app_name, step, input, params, bagage, repo)
+          "delete_entity" -> delete_entity(app_name, step, input, params, bagage, repo)
         end
       end)
     end)
@@ -98,38 +98,38 @@ defmodule Erm.Core.Action do
     Erm.Persistence.Ecto.save_entity(app_name, new_ent, repo)
   end
 
-  defp update_entity(app_name, step, params, bagage, repo) do
+  defp update_entity(app_name, step, input, params, bagage, repo) do
     data = find_data(step["data"], params, bagage)
-    id = find_ft(step["id"], params, bagage)
+    id = find_ft(step["id"], input, params, bagage)
     up_entity = Entity.new(%{type: step["type"], data: data, id: id})
     Erm.Persistence.Ecto.save_entity(app_name, up_entity, repo)
   end
 
-  defp add_relation(app_name, step, params, bagage, repo) do
-    from = find_ft(step["from"], params, bagage)
-    to = find_ft(step["to"], params, bagage)
+  defp add_relation(app_name, step, input, params, bagage, repo) do
+    from = find_ft(step["from"], input, params, bagage)
+    to = find_ft(step["to"], input, params, bagage)
     data = find_data(step["data"], params, bagage)
     new_rel = Relation.new(%{type: step["type"], from: from, to: to, data: data})
     Erm.Persistence.Ecto.save_relation(app_name, new_rel, repo)
   end
 
-  defp update_relation(app_name, step, params, bagage, repo) do
+  defp update_relation(app_name, step, input, params, bagage, repo) do
     data = find_data(step["data"], params, bagage)
-    from = find_ft(step["from"], params, bagage)
-    to = find_ft(step["to"], params, bagage)
+    from = find_ft(step["from"], input, params, bagage)
+    to = find_ft(step["to"], input, params, bagage)
     update_rel = Relation.new(%{type: step["type"], from: from, to: to, data: data})
     Erm.Persistence.Ecto.save_relation(app_name, update_rel, repo)
   end
 
-  defp delete_relation(app_name, step, params, bagage, repo) do
-    from = find_ft(step["from"], params, bagage)
-    to = find_ft(step["to"], params, bagage)
+  defp delete_relation(app_name, step, input, params, bagage, repo) do
+    from = find_ft(step["from"], input, params, bagage)
+    to = find_ft(step["to"], input, params, bagage)
     type = step["type"]
     Erm.Persistence.Ecto.remove_relation(app_name, type, from, to, repo)
   end
 
-  defp delete_entity(app_name, step, params, bagage, repo) do
-    id = find_ft(step["id"], params, bagage)
+  defp delete_entity(app_name, step, input, params, bagage, repo) do
+    id = find_ft(step["id"], input, params, bagage)
     Erm.Persistence.Ecto.remove_entity(app_name, id, repo)
   end
 
@@ -154,9 +154,9 @@ defmodule Erm.Core.Action do
 
   defp else_find_data_in_input(data, _data_name, _params), do: data
 
-  defp find_ft(ft_name, params, bg) do
+  defp find_ft(ft_name, input, params, bg) do
     find_ft_in_bg(ft_name, bg)
-    |> else_find_ft_in_input(ft_name, params)
+    |> else_find_ft_in_input(ft_name, input, params)
   end
 
   defp find_ft_in_bg(ft_name, bg) do
@@ -166,12 +166,19 @@ defmodule Erm.Core.Action do
     end
   end
 
-  defp else_find_ft_in_input(nil, ft_name, params) do
+  defp else_find_ft_in_input(nil, ft_name, input, params) do
     case Map.get(params, ft_name) do
-      nil -> nil
-      ft -> ft
+      nil ->
+        nil
+
+      ft ->
+        case Map.get(input, ft_name) do
+          "id" -> ft
+          "map" -> ft["id"]
+          _ -> ft
+        end
     end
   end
 
-  defp else_find_ft_in_input(data, _ft_name, _params), do: data
+  defp else_find_ft_in_input(data, _input, _ft_name, _params), do: data
 end
