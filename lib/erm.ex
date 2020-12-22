28bcb255d481e1cad838783d00049c18308e323e
tulinmola <tulinmola@gmail.com>
2020-12-22 18:09:32 +0100

    Add Erm external API

--- lib/erm.ex
+++ lib/erm.ex
@@ -3,16 +3,13 @@ defmodule Erm do
   Documentation for `Erm`.
   """
 
-  @doc """
-  Hello world.
+  alias Erm.Boundary.ErmApi
 
-  ## Examples
-
-      iex> Erm.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  defdelegate run_action(app_name, action_name, params), to: ErmApi
+  defdelegate run_action!(app_name, action_name, params), to: ErmApi
+  defdelegate list_entities(app_name, type, equality_field_values), to: ErmApi
+  defdelegate list_entities_by_relation(app_name, type, direction, id), to: ErmApi
+  defdelegate get_entity(app_name, uuid), to: ErmApi
+  defdelegate list_relations(app_name, type, properties), to: ErmApi
+  defdelegate get_relation(app_name, type, from, to), to: ErmApi
 end
--- test/erm_test.exs
+++ test/erm_test.exs
@@ -1,8 +1,4 @@
 defmodule ErmTest do
   use ExUnit.Case
   doctest Erm
-
-  test "greets the world" do
-    assert Erm.hello() == :world
-  end
 end
